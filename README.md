faba865d526753fcfe2ed1121e53b44bb385a670
Girish Shenoy <girish@vishwayon.com>
2021-01-21 10:36:44 +0530

    key-token-login example created

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # coreerp-tpa-example
+
+This project contains all the example code required by third party applications to successfully login and open documents/masters in coreERP.
+The minimum supported version of coreERP is required to be branched from dev-bs4.
+
+## Example 1: Key Token Login (Path: key-token-login)
+
+This example code explains the Automatic login process in coreERP using the Auth-Key, Auth-Secret and User-Secret
+
+It uses the following files:
+    1. Server.php   - This contains the server code
+    2. Index.html   - The html page served to the client
+    3. client.js    - Client javascript to call server and open new window on successful authentication
+
+
+### License
+These examples are licensed under [MIT](https://github.com/vishwayon/coreerp-tpa-example/blob/develop/LICENSE).
---
+++ key-token-login/Server.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @link http://www.vishwayon.com/
+ * @copyright Copyright (c) 2020 Vishwayon Software Pvt Ltd
+ * @license MIT
+ */
+
+require '../vendor/autoload.php';
+
+// Never Pass the following variables to the client in HTML or JS. Always retain them securely on the server
+$auth_key = '71faabb1d7b7bf966c0188e0918a87f3';          // This is the Authentication ID assigned to your application
+$auth_secret = 'f035f45bfa3de3dc999476f70df9b790';      // This is the Authentication Secret assigned to your application
+$user_secret = '161ad23e58d55d8065e45c45b3b04fa9';      // User needs to generate this secret from his User Profile
+
+// Application Paramertes
+$user_name = 'Support_user';        // The logon user_name in coreERP
+$coreErpUrl = 'http://172.17.0.4/core-erp/index.php';      // coreERP published site address
+        
+// Create a Ghuzzle Client instance and call authentication
+$client = new GuzzleHttp\Client([
+    // Base URI is used with relative requests
+    'base_uri' => $coreErpUrl,
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+]);
+
+// Prepare Authorization header
+$ah = base64_encode($auth_key . ':' . $auth_secret);
+// Prepare Request with headers
+$request = new \GuzzleHttp\Psr7\Request('GET', '?r=auth/key-login&user=' . $user_name, [
+    'Authorization' => $ah,
+    'Usersecret' => base64_encode($user_secret)
+]);
+// Process Response
+$response = $client->send($request);
+$result = json_decode($response->getBody());
+if ($result->status == 'OK') {
+    $result->url = $coreErpUrl;
+    $result->route = 'auth/relay-login';
+}
+echo json_encode($result) . PHP_EOL;
+
+
+

eb3edf4585d6367c2bbdea8b8992fc2685b5b6c7
vspl-devd <devadatta.wakalkar@coreerp.com>
2021-09-16 23:25:16 +0530

    WIP: report api

--- key-token-login/client.js
+++ key-token-login/client.js
@@ -39,6 +39,52 @@ ktl = (window.ktl || {});
     }
     ktl.login = login;
     
+    function print() {
+        $.ajax({
+            url: 'ReportServer.php',
+            method: 'GET',
+            dataType: 'json',
+            data: {'doc-type': 'DSP','doc-id': ($('#doc-id').val() != '' ?  $('#doc-id').val() : '-1')},
+            success: function(result) {
+                console.log(result);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+    ktl.print = print;
+    
+    function print2(target) {
+        $.ajax({
+            url: 'ReportServer.php',
+            method: 'GET',
+            dataType: 'json',
+            success: function (result) {
+                if (result.status == 'OK') {
+                    var uri = result.url + '?' + $.param({
+                        r: result.route,
+                        'auth-token': result['auth-token'],
+                        'core-sessionid': result['core-sessionid'],
+                        'doc-type': 'DSP',
+                        'doc-id': ($('#doc-id').val() != '' ? $('#doc-id').val() : '-1'), 
+                    });
+                    if (target) {
+                        $(target).attr('src', uri);
+                    } else {
+                        window.open(uri);
+                    }
+                } else {
+                    alert(result.msg);
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        })
+    }
+    ktl.print2 = print2;
+    
 } (window.ktl));
 
 
